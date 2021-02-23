487c79a562a92008cb2ca4bb552744e838948323
Gabriel Yeo <gabriel.yfy@gmail.com>
2021-02-23 18:45:16 +0800

    Try remove memory leak

--- solution.cpp
+++ solution.cpp
@@ -30,6 +30,124 @@ void solution::random(const problem &input)
 
 // Solomon's I1 insertion heuristic (1987)
 // Ref.: "Algorithms for the Vehicle Routing and Scheduling Problems with Time Window Constraints"
+// void solution::solomon(const problem &input, int insertionCriteria, double mu, double lambda, double alpha_1)
+// {
+// 	clear();
+
+// 	set<int> unrouted;
+// 	for (int id = 1; id <= input.getNumCusto(); id++)
+// 		unrouted.insert(id);
+
+// 	route *initialRoute = new route();
+// 	customer depot = input[0];
+// 	visit depotStop = visit(depot, 0);
+// 	initialRoute->visits.push_back(depotStop);
+// 	initialRoute->visits.push_back(depotStop);
+
+// 	initialRoute->capacity = input.getCapacity();
+
+// 	vector<route *> newRoutes;
+// 	newRoutes.push_back(initialRoute);
+
+// 	while (!unrouted.empty())
+// 	{
+// 		int bestUnroutedCustomer = 0;
+// 		double bestFitness = 0;
+// 		route *bestRoute;
+// 		int bestPositionOnRoute = -1;
+// 		bool hasUpdated = false;
+
+// 		for (auto currCustomer : unrouted)
+// 		{
+// 			for (auto route : newRoutes)
+// 			{
+// 				for (int prev = 0; prev < route->visits.size() - 1; prev++)
+// 				{
+// 					bool isFeasible = route->check_push_forward(prev, input[currCustomer], input);
+// 					if (isFeasible)
+// 					{
+// 						double fitness = route->get_fitness(prev, input[currCustomer], input, mu, lambda, alpha_1);
+// 						if ((hasUpdated == false) || (fitness < bestFitness))
+// 						{
+// 							hasUpdated = true;
+// 							bestFitness = fitness;
+// 							bestRoute = route;
+// 							bestPositionOnRoute = prev;
+// 							bestUnroutedCustomer = currCustomer;
+// 						}
+// 					}
+// 				}
+// 			}
+// 		}
+// 		if (hasUpdated)
+// 		{
+// 			if (!bestRoute->set_push_forward(bestPositionOnRoute, input[bestUnroutedCustomer], input))
+// 			{
+// 				// Should return true after successful insertion
+// 				throw "Insertion failure";
+// 			}
+// 			unrouted.erase(bestUnroutedCustomer);
+// 		}
+// 		else
+// 		{
+// 			route *newRoute = new route();
+// 			newRoute->visits.push_back(depotStop);
+// 			newRoute->visits.push_back(depotStop);
+// 			newRoute->capacity = input.getCapacity();
+// 			newRoutes.push_back(newRoute);
+
+// 			int bestCustomer = -1;
+// 			double bestCustomerFitness = 0;
+// 			bool set = false;
+
+// 			if (insertionCriteria == 1)
+// 			{
+// 				for (auto currCustomer : unrouted)
+// 				{
+// 					// farthest customer
+// 					double currDist = input.getDistance(0, currCustomer);
+// 					if ((!set) || currDist > bestCustomerFitness)
+// 					{
+// 						set = true;
+// 						bestCustomerFitness = currDist;
+// 						bestCustomer = currCustomer;
+// 					}
+// 				}
+// 			}
+// 			else if (insertionCriteria == 2)
+// 			{
+// 				for (auto currCustomer : unrouted)
+// 				{
+// 					// purely b_ju - b_j
+// 					double currFitness = newRoute->get_fitness(0, input[currCustomer], input, 1, 0, 0);
+// 					if ((!set) || currFitness < bestCustomerFitness)
+// 					{
+// 						set = true;
+// 						bestCustomerFitness = currFitness;
+// 						bestCustomer = currCustomer;
+// 					}
+// 				}
+// 			}
+
+// 			if (set)
+// 			{
+// 				// cout << "Inserting customer: " << bestCustomer << " with fitness: " << bestCustomerFitness << endl;
+// 				vector<visit>::iterator it = newRoute->visits.begin();
+// 				newRoute->set_push_forward(0, input[bestCustomer], input);
+// 				unrouted.erase(bestCustomer);
+// 			}
+// 		}
+// 	}
+
+// 	for (auto route : newRoutes)
+// 	{
+// 		routes.push_back(*route);
+// 		totalDistance += route->distance;
+// 	}
+// }
+
+//////////////////////////
+
 void solution::solomon(const problem &input, int insertionCriteria, double mu, double lambda, double alpha_1)
 {
 	clear();
@@ -38,40 +156,50 @@ void solution::solomon(const problem &input, int insertionCriteria, double mu, d
 	for (int id = 1; id <= input.getNumCusto(); id++)
 		unrouted.insert(id);
 
-	route *initialRoute = new route();
+	// route *initialRoute = new route();
+	// customer depot = input[0];
+	// visit depotStop = visit(depot, 0);
+	// initialRoute->visits.push_back(depotStop);
+	// initialRoute->visits.push_back(depotStop);
+	// initialRoute->capacity = input.getCapacity();
+
+	// vector<route *> newRoutes;
+	routes.emplace_back();
+	routes.emplace_back();
+
 	customer depot = input[0];
 	visit depotStop = visit(depot, 0);
-	initialRoute->visits.push_back(depotStop);
-	initialRoute->visits.push_back(depotStop);
-
-	initialRoute->capacity = input.getCapacity();
-
-	vector<route *> newRoutes;
-	newRoutes.push_back(initialRoute);
+	routes[0].visits.push_back(depotStop);
+	routes[0].visits.push_back(depotStop);
+	routes[0].capacity = input.getCapacity();
+	routes[1].visits.push_back(depotStop);
+	routes[1].visits.push_back(depotStop);
+	routes[1].capacity = input.getCapacity();
 
 	while (!unrouted.empty())
 	{
 		int bestUnroutedCustomer = 0;
 		double bestFitness = 0;
-		route *bestRoute = initialRoute;
+		route *bestRoute;
 		int bestPositionOnRoute = -1;
 		bool hasUpdated = false;
 
 		for (auto currCustomer : unrouted)
 		{
-			for (auto route : newRoutes)
+			for (int i = 0; i < routes.size(); i++)
 			{
-				for (int prev = 0; prev < route->visits.size() - 1; prev++)
+				// route routes[i] = routes[i];
+				for (int prev = 0; prev < routes[i].visits.size() - 1; prev++)
 				{
-					bool isFeasible = route->check_push_forward(prev, input[currCustomer], input);
+					bool isFeasible = routes[i].check_push_forward(prev, input[currCustomer], input);
 					if (isFeasible)
 					{
-						double fitness = route->get_fitness(prev, input[currCustomer], input, mu, lambda, alpha_1);
+						double fitness = routes[i].get_fitness(prev, input[currCustomer], input, mu, lambda, alpha_1);
 						if ((hasUpdated == false) || (fitness < bestFitness))
 						{
 							hasUpdated = true;
 							bestFitness = fitness;
-							bestRoute = route;
+							bestRoute = &routes[i];
 							bestPositionOnRoute = prev;
 							bestUnroutedCustomer = currCustomer;
 						}
@@ -90,11 +218,15 @@ void solution::solomon(const problem &input, int insertionCriteria, double mu, d
 		}
 		else
 		{
-			route *newRoute = new route();
-			newRoute->visits.push_back(depotStop);
-			newRoute->visits.push_back(depotStop);
-			newRoute->capacity = input.getCapacity();
-			newRoutes.push_back(newRoute);
+			// route *newRoute = new route();
+			routes.emplace_back();
+			routes[routes.size() - 1].visits.push_back(depotStop);
+			routes[routes.size() - 1].visits.push_back(depotStop);
+			routes[routes.size() - 1].capacity = input.getCapacity();
+			// newRoute->visits.push_back(depotStop);
+			// newRoute->visits.push_back(depotStop);
+			// newRoute->capacity = input.getCapacity();
+			// newRoutes.push_back(newRoute);
 
 			int bestCustomer = -1;
 			double bestCustomerFitness = 0;
@@ -119,7 +251,7 @@ void solution::solomon(const problem &input, int insertionCriteria, double mu, d
 				for (auto currCustomer : unrouted)
 				{
 					// purely b_ju - b_j
-					double currFitness = newRoute->get_fitness(0, input[currCustomer], input, 1, 0, 0);
+					double currFitness = routes[routes.size() - 1].get_fitness(0, input[currCustomer], input, 1, 0, 0);
 					if ((!set) || currFitness < bestCustomerFitness)
 					{
 						set = true;
@@ -132,16 +264,16 @@ void solution::solomon(const problem &input, int insertionCriteria, double mu, d
 			if (set)
 			{
 				// cout << "Inserting customer: " << bestCustomer << " with fitness: " << bestCustomerFitness << endl;
-				vector<visit>::iterator it = newRoute->visits.begin();
-				newRoute->set_push_forward(0, input[bestCustomer], input);
+				vector<visit>::iterator it = routes[routes.size() - 1].visits.begin();
+				routes[routes.size() - 1].set_push_forward(0, input[bestCustomer], input);
 				unrouted.erase(bestCustomer);
 			}
 		}
 	}
 
-	for (auto route : newRoutes)
+	for (auto route : routes)
 	{
-		routes.push_back(*route);
-		totalDistance += route->distance;
+		route.print(stdout);
+		totalDistance += route.distance;
 	}
 }
